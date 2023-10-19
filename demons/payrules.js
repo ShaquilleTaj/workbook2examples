1a7347c91a7e89df65117a5cd6e1ab0ea59fe469
Shaquille Taj <staj00647@gmail.com>
2023-10-19 10:50:56 -0400

    payrules exercise

---
+++ demons/payrules.js
@@ -0,0 +1,10 @@
+let payrate = 35;
+let hoursworked = 60;
+if (hoursworked < 40){
+    console.log(hoursworked * payrate);
+    elif (hoursworked = 40);
+    console.log(hoursworked * payrate);
+   
+}
+ else (hoursworked > 40);
+  console.log(hoursworked * 1.5 * payrate);
