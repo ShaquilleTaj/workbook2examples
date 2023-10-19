9919fdda928b9417a5e94b5920ed53a04cf285df
Shaquille Taj <staj00647@gmail.com>
2023-10-19 09:17:47 -0400

    finished workbook

---
+++ math scripts/wb2-exercises/conversion-tests.js
@@ -0,0 +1,24 @@
+"use strict";
+var a = " 101.1  ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+parseInt(a);
+parseInt(b);
+parseInt(c);
+parseInt(d);
+console.log(parseInt(a),parseInt(b),parseInt(c),parseInt(d));
+parseFloat(a);
+parseFloat(b);
+parseFloat(c);
+parseFloat(d);
+console.log(parseFloat(a),
+parseFloat(b),
+parseFloat(c),
+parseFloat(d),)
+Number(a);
+Number(b);
+Number(c);
+Number(d);
+console.log(Number(a),Number(b),Number(c),Number(d));
+console.log(+a, +b, +c, +d);
